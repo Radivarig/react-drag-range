edb0d0e7021646fb18cade2344e94ad23e7604fa
Radivarig <reslav.hollos@gmail.com>
2016-09-08 00:39:34 +0000

    (fix) do not affect adjacent on single click

--- src/Examples/DragRangeHorizontalSliders.jsx
+++ src/Examples/DragRangeHorizontalSliders.jsx
@@ -5,12 +5,15 @@ const DragRangeViewer = React.createClass({
   getInitialState() {
     let s = {
       slidersCount: 15,
+      isFirstClick: true,
     }
     for (var i = 0; i < s.slidersCount; ++i)
       s['value_' +i] = 50
     console.log (s)
     return s
   },
+
+  lastDelta: 0,
 
   getHorSlider(n) {
     const height = 200
@@ -46,20 +49,21 @@ const DragRangeViewer = React.createClass({
     const onChange = (value) => {
       const currValue = this.state['value_' + n]
       const delta =  currValue - value
-      s = {}
+      s = {isFirstClick: false}
       const getMultiplier = (distance) => Math.pow (0.3, distance)
 
-      for (var j = 0; j < this.state.slidersCount; ++j) {
-        if (j !== n) {
-          const adjacentValue = this.state['value_' + j]
-          const adjacentDelta = currValue - adjacentValue
+      if ( ! this.state.isFirstClick) {
+        for (var j = 0; j < this.state.slidersCount; ++j) {
+          if (j !== n) {
+            const adjacentValue = this.state['value_' + j]
+            const adjacentDelta = currValue - adjacentValue
 
-          s['value_' + j] = adjacentValue + getMultiplier(Math.abs(n - j)) * adjacentDelta
+            s['value_' + j] = adjacentValue + getMultiplier(Math.abs(n - j)) * adjacentDelta
+          }
         }
-
       }
-      s['value_' + n] = value
 
+      s['value_' + n] = value
       this.setState(s)
     }
 
@@ -71,6 +75,7 @@ const DragRangeViewer = React.createClass({
           getTarget={()=>this.refs['target_' + n]}
           value={value}
           onChange={onChange}
+          onMouseUp={()=>this.setState({isFirstClick: true})}
         >
           <div style={{display: 'inline-block'/*, border: '1px solid red'*/}} ref={'target_' + n}>
             <div style={sliderStyle}>
