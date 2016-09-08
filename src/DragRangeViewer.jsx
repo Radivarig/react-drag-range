8b05857e2597ddc588c44f764f1900b95a6fcbe2
Radivarig <reslav.hollos@gmail.com>
2016-09-08 00:53:38 +0000

    (add) horizontal sliders equalizer example

--- src/DragRangeViewer.jsx
+++ src/DragRangeViewer.jsx
@@ -5,30 +5,44 @@ const {
 } = require('react-bootstrap')
 
 const DragRangeModifyEachProp = require('./Examples/DragRangeModifyEachProp.jsx')
+const DragRangeTangleJS = require('./Examples/DragRangeTangleJS.jsx')
 const DragRangeSimplePercent = require('./Examples/DragRangeSimplePercent.jsx')
 const DragRangeHashProgressBar = require('./Examples/DragRangeHashProgressBar.jsx')
 const DragRangeCssHighlight = require('./Examples/DragRangeCssHighlight.jsx')
 const DragRangeAnalogStick = require('./Examples/DragRangeAnalogStick.jsx')
 const DragRangeKnobControl = require('./Examples/DragRangeKnobControl.jsx')
 const DragRangeSliderVertical = require('./Examples/DragRangeSliderVertical.jsx')
-
+const DragRangeHorizontalSliders = require('./Examples/DragRangeHorizontalSliders.jsx')
 
 const DragRangeViewer = React.createClass({
   render() {
     const rowStyle = {
       backgroundColor: '#ddd',
       textAlign: 'center',
-      margin: 20,
+      margin: 10,
+      padding: 20,
+      boxShadow: '0px 5px 10px #888',
     }
     return (
-      <Grid>
+      <Grid style={{textAlign: 'center'}}>
+
+        <h1>React Drag Range</h1>
+
+        <Row style={rowStyle}>
+          Drag Range is a React component that provides detection for click/drag rate changes and callbacks with value/delta/percent changes for both X and Y axis.
+        </Row>
+
+        <h1>Examples</h1>
+
         <Row style={rowStyle}><DragRangeModifyEachProp/></Row>
+        <Row style={rowStyle}><DragRangeTangleJS/></Row>
         <Row style={rowStyle}><DragRangeSimplePercent/></Row>
         <Row style={rowStyle}><DragRangeHashProgressBar/></Row>
         <Row style={rowStyle}><DragRangeCssHighlight/></Row>
         <Row style={rowStyle}><DragRangeAnalogStick/></Row>
         <Row style={rowStyle}><DragRangeKnobControl/></Row>
         <Row style={rowStyle}><DragRangeSliderVertical/></Row>
+        <Row style={rowStyle}><DragRangeHorizontalSliders/></Row>
       </Grid>
     )
   }
