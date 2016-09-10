a5bfd7336bd20bdd9a947ea21be9711227102e02
Radivarig <reslav.hollos@gmail.com>
2016-09-10 20:19:59 +0000

    (add) tanglejs example

---
+++ src/Examples/DragRangeTangleJS.jsx
@@ -0,0 +1,60 @@
+const React = require('react')
+const DragRange = require('../DragRange.jsx')
+
+const DragRangeViewer = React.createClass({
+  getInitialState() {
+    return {
+      value: 3,
+    }
+  },
+
+  setHover(value) {
+    return (e) => {
+      this.setState({hover: value})
+    }
+  },
+
+  render() {
+    const tangleStyle = {
+      cursor: 'ew-resize',
+      borderBottom: '1px dotted',
+      color: 'royalBlue',
+      position: 'relative',
+    }
+
+    // const hoverStyle ={
+    //   position: 'absolute',
+    //   left: '50%',
+    //   fontSize: '0.8em',
+    //   transform: 'translate(-50%, -50%)',
+    // }
+    // const hoverText = this.state.hover ? <span style={hoverStyle}>drag</span> : ''
+    const hoverText = ''
+ 
+    return (
+      <div>
+        This is a drag range component inspired by <a href='http://worrydream.com/Tangle/' target='_blank'>TangleJS</a>.
+        <br/>
+        If you eat &nbsp;
+        <DragRange
+          value={this.state.value}
+          onChange={(value) => this.setState({value})}
+          min={0}
+        >
+          <span style={tangleStyle}>
+            {hoverText}
+            <span ref='hover'
+              onMouseOver={this.setHover(true)}
+              onMouseOut={this.setHover(false)}
+            >
+              {this.state.value} cookies
+            </span>
+          </span>
+        </DragRange>
+        &nbsp; you consume {this.state.value * 50} calories.
+      </div>
+    )
+  }
+})
+
+module.exports = DragRangeViewer
