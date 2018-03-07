4832a17f0bb2b5510dde1aca0e0b0547d51a8da8
Radivarig <reslav.hollos@gmail.com>
2018-03-08 00:04:33 +0100

    Update syntax to es6, disabled all examples

--- src/DragRangeViewer.jsx
+++ src/DragRangeViewer.jsx
@@ -1,5 +1,6 @@
-const React = require('react')
+import React from 'react'
 
+/*
 const DragRangeModifyEachProp = require('./Examples/DragRangeModifyEachProp.jsx')
 const DragRangeTangleJS = require('./Examples/DragRangeTangleJS.jsx')
 const DragRangeSimplePercent = require('./Examples/DragRangeSimplePercent.jsx')
@@ -9,39 +10,37 @@ const DragRangeAnalogStick = require('./Examples/DragRangeAnalogStick.jsx')
 const DragRangeKnobControl = require('./Examples/DragRangeKnobControl.jsx')
 const DragRangeSliderVertical = require('./Examples/DragRangeSliderVertical.jsx')
 const DragRangeHorizontalSliders = require('./Examples/DragRangeHorizontalSliders.jsx')
-
-const DragRangeViewer = React.createClass({
-  render() {
-    const rowStyle = {
-      backgroundColor: '#eee',
-      textAlign: 'center',
-      margin: 10,
-      padding: 20,
-      boxShadow: '0px 5px 10px #888',
-    }
-    return (
-      <div style={{textAlign: 'center'}}>
-
-        <h1>React Drag Range</h1>
-
-        <div style={rowStyle}>
-          Drag Range is a React component that provides detection for click/drag rate changes and callbacks with value/delta/percent changes for both X and Y axis.
-        </div>
-
-        <h1>Examples</h1>
-
-        <div style={rowStyle}><DragRangeModifyEachProp/></div>
-        <div style={rowStyle}><DragRangeTangleJS/></div>
-        <div style={rowStyle}><DragRangeSimplePercent/></div>
-        <div style={rowStyle}><DragRangeHashProgressBar/></div>
-        <div style={rowStyle}><DragRangeCssHighlight/></div>
-        <div style={rowStyle}><DragRangeAnalogStick/></div>
-        <div style={rowStyle}><DragRangeKnobControl/></div>
-        <div style={rowStyle}><DragRangeSliderVertical/></div>
-        <div style={rowStyle}><DragRangeHorizontalSliders/></div>
-      </div>
-    )
+*/
+export default (props) => {
+  const rowStyle = {
+    backgroundColor: '#eee',
+    textAlign: 'center',
+    margin: 10,
+    padding: 20,
+    boxShadow: '0px 5px 10px #888',
   }
-})
+  return (
+    <div style={{textAlign: 'center'}}>
 
-module.exports = DragRangeViewer
+      <h1>React Drag Range</h1>
+
+      <div style={rowStyle}>
+        Drag Range is a React component that provides detection for click/drag rate changes and callbacks with value/delta/percent changes for both X and Y axis.
+      </div>
+
+      <h1>Examples</h1>
+
+    </div>
+  )
+}
+/*
+      <div style={rowStyle}><DragRangeModifyEachProp/></div>
+      <div style={rowStyle}><DragRangeTangleJS/></div>
+      <div style={rowStyle}><DragRangeSimplePercent/></div>
+      <div style={rowStyle}><DragRangeHashProgressBar/></div>
+      <div style={rowStyle}><DragRangeCssHighlight/></div>
+      <div style={rowStyle}><DragRangeAnalogStick/></div>
+      <div style={rowStyle}><DragRangeKnobControl/></div>
+      <div style={rowStyle}><DragRangeSliderVertical/></div>
+      <div style={rowStyle}><DragRangeHorizontalSliders/></div>
+*/
