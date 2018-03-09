4877b6dc6a2dfed0580a556165a712a8d2c4c46a
Radivarig <reslav.hollos@gmail.com>
2018-03-10 00:58:15 +0100

    Add prepublish with peer deps externals

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,21 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin")
+const packageJson = require("./package.json")
+
+const mainFile = "DragRange.jsx"
 
 module.exports = {
   entry: {
-    main: __dirname + "/src/DragRange.jsx",
+    main: __dirname + "/src/" + mainFile,
   },
 
   output: {
     filename: "[name].js",
     path: __dirname + "/dist",
-    library: "reactDragRange",
+    library: mainFile.substring (0, mainFile.indexOf(".")),
     libraryTarget: "umd",
   },
+
+  externals: Object.keys(packageJson.peerDependencies),
 
   devtool: "source-map",
 
